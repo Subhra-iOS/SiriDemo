baf4a399a400a2fabdc73f836777ea2cfe6a4c03
Subhra Roy <subhra.roy84@yahoo.in>
2020-10-13 17:21:38 +0530

    update as parameter

--- SiriIntentDemo/Shared/TaskIntentHandler.swift
+++ SiriIntentDemo/Shared/TaskIntentHandler.swift
@@ -6,9 +6,18 @@
 //
 
 import Foundation
+import Intents
 import os.log
 
 public class TaskIntentHandler: NSObject, TaskIntentHandling {
+    
+    public func resolveOrderType(for intent: TaskIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        if intent.orderType == nil{
+            completion(INStringResolutionResult.disambiguation(with: ["Print Order","Pickup","Request Quote"]))
+        }else{
+            completion(INStringResolutionResult.success(with: intent.orderType ?? ""))
+        }
+    }
 
     public func confirm(intent: TaskIntent, completion: @escaping (TaskIntentResponse) -> Void) {
         os_log(#function)
